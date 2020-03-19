54ab90fc6a255bcb03c9b3fe9511433bdbf7cd94
mur4ik18 <sasha.bazalev1@gmail.com>
2020-03-19 22:55:34 +0200

    add file save(file.txt), add finder

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.ebay.com/b/Video-Game-Consoles/139971/bn_320033"
+#   https://www.ebay.com/sch/i.html?_from=R40  (&_nkw=GTX+1060 - what we find)   (&_pgn=1 - pages)
 
-r = requests.get(URL)
-html = BeautifulSoup(r.content, 'html.parser') 
+find = input("What we find? ")
+findList = find.split() 
+WellList = '+'.join(findList)
+URL = "https://www.ebay.com/sch/i.html?_from=R40&_nkw=" + str(WellList) + "&_pgn="
+#URL = "https://www.ebay.com/b/Video-Game-Consoles/139971/bn_320033?rt=nc&_pgn="
+pagesNum = input("How much pages you need?")
 
 try:
     shell = ".s-item"
@@ -27,31 +31,61 @@ try:
 except:
     print('')
 
+f = open('output.txt', 'w')
+
 s=0
-for el in html.select(shell):
+for i in range(1, int(pagesNum)):
     try:
-        t = el.select(title)
-        print(t[0].text)
+        r = requests.get(URL+str(i))
+        html = BeautifulSoup(r.content, 'html.parser') 
     except:
-        print("")
-    try:
-        p = el.select(price)
-        print(p[0].text)
-    except:
-        print("")
-    try:
-        sold = el.select(solds)
-        print(sold[0].text)
-    except:
-        print("")       
-    try:
-        star = el.secect(stars)
-        print(star[0].text)
-    except:
-        print("")
+        print("end work")
+        print(s)
+        break
+    for el in html.select(shell):
+        # title
+        try:
+            t = el.select(title)
+            print(t[0].text)
+            f.write(t[0].text)
+            f.write('\n')
+        except:
+            print("")
+        # link
+        try:
+            link = el.select('.s-item__link')
+            print(link[0].get('href'))
+            f.write(link[0].get('href'))
+            f.write('\n')
+        except:
+            print("")
         
-        
-        
+        # price
+        try:
+            p = el.select(price)
+            print(p[0].text)
+            f.write(p[0].text)
+            f.write('\n')
+        except:
+            print("")
+        # sold
+        try:
+            sold = el.select(solds)
+            print(sold[0].text)
+            f.write(sold[0].text)
+            f.write('\n')
+        except:
+            print("")       
+        # rate
+        try:
+            star = el.secect(stars)
+            print(star[0].text)
+            f.write(star[0].text)
+        except:
+            print("")
+        s+=1
+        print(s)
+        f.write('--------------')
+        f.write('\n')
     
-    s+=1
-    print(s)
+f.close()
